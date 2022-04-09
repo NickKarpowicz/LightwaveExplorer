8508d3ee1042dcb66a675a808ef45ef51c662aa5
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2022-04-09 21:43:45 +0200

    Cleanup

--- TestFile.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import numpy as np
-dt = 5.000000e-16
-dz = 2.500000e-08
-dx = 1.000000e-06
-TestFile_ExtIn = np.reshape(np.fromfile("TestFile_ExtIn.dat",dtype=np.double)[0:1048576],(1024,512,2),order='F')
-TestFile_ExtOut = np.reshape(np.fromfile("TestFile_ExtOut.dat",dtype=np.double)[0:1048576],(1024,512,2),order='F')
