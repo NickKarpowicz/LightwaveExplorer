13ceb1aaafceafc96d17c7da6e61e6b0574700d4
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2022-09-26 13:11:38 +0200

    Fitting stability fix

--- LightwaveExplorerUtilities.cpp
+++ LightwaveExplorerUtilities.cpp
@@ -9,10 +9,78 @@
 //This is part of the Dlib library
 // Copyright (C) 2006  Davis E. King (davis@dlib.net)
 // License: Boost Software License   See LICENSE.txt for the full license.
+//#include "dlib/test_for_odr_violations.cpp"
+//#include "dlib/threads/thread_pool_extension.cpp"
+//#include "dlib/threads/async.cpp"
+//#include "dlib/global_optimization/global_function_search.cpp"
+
+
+#ifndef DLIB_ALL_SOURCe_
+#define DLIB_ALL_SOURCe_
+
+#if defined(DLIB_ALGs_) || defined(DLIB_PLATFORm_)
+#include "dlib/dlib_basic_cpp_build_tutorial.txt"
+#endif
+
+// ISO C++ code
+#include "dlib/base64/base64_kernel_1.cpp"
+#include "dlib/bigint/bigint_kernel_1.cpp"
+#include "dlib/bigint/bigint_kernel_2.cpp"
+#include "dlib/bit_stream/bit_stream_kernel_1.cpp"
+#include "dlib/entropy_decoder/entropy_decoder_kernel_1.cpp"
+#include "dlib/entropy_decoder/entropy_decoder_kernel_2.cpp"
+#include "dlib/entropy_encoder/entropy_encoder_kernel_1.cpp"
+#include "dlib/entropy_encoder/entropy_encoder_kernel_2.cpp"
+#include "dlib/md5/md5_kernel_1.cpp"
+#include "dlib/tokenizer/tokenizer_kernel_1.cpp"
+#include "dlib/unicode/unicode.cpp"
 #include "dlib/test_for_odr_violations.cpp"
+
+#ifndef DLIB_ISO_CPP_ONLY
+// Code that depends on OS specific APIs
+
+// include this first so that it can disable the older version
+// of the winsock API when compiled in windows.
+#include "dlib/sockets/sockets_kernel_1.cpp"
+#include "dlib/bsp/bsp.cpp"
+
+#include "dlib/dir_nav/dir_nav_kernel_1.cpp"
+#include "dlib/dir_nav/dir_nav_kernel_2.cpp"
+#include "dlib/dir_nav/dir_nav_extensions.cpp"
+#include "dlib/linker/linker_kernel_1.cpp"
+#include "dlib/logger/extra_logger_headers.cpp"
+#include "dlib/logger/logger_kernel_1.cpp"
+#include "dlib/logger/logger_config_file.cpp"
+#include "dlib/misc_api/misc_api_kernel_1.cpp"
+#include "dlib/misc_api/misc_api_kernel_2.cpp"
+#include "dlib/sockets/sockets_extensions.cpp"
+#include "dlib/sockets/sockets_kernel_2.cpp"
+#include "dlib/sockstreambuf/sockstreambuf.cpp"
+#include "dlib/sockstreambuf/sockstreambuf_unbuffered.cpp"
+#include "dlib/server/server_kernel.cpp"
+#include "dlib/server/server_iostream.cpp"
+#include "dlib/server/server_http.cpp"
+#include "dlib/threads/multithreaded_object_extension.cpp"
+#include "dlib/threads/threaded_object_extension.cpp"
+#include "dlib/threads/threads_kernel_1.cpp"
+#include "dlib/threads/threads_kernel_2.cpp"
+#include "dlib/threads/threads_kernel_shared.cpp"
 #include "dlib/threads/thread_pool_extension.cpp"
 #include "dlib/threads/async.cpp"
+#include "dlib/timer/timer.cpp"
+#include "dlib/stack_trace.cpp"
+
+#include "dlib/cuda/cpu_dlib.cpp"
+#include "dlib/cuda/tensor_tools.cpp"
+#include "dlib/data_io/image_dataset_metadata.cpp"
+#include "dlib/data_io/mnist.cpp"
+#include "dlib/data_io/cifar.cpp"
+#include "dlib/svm/auto.cpp"
 #include "dlib/global_optimization/global_function_search.cpp"
+#include "dlib/filtering/kalman_filter.cpp"
+#endif // DLIB_ISO_CPP_ONLY
+#define DLIB_ALL_SOURCE_END
+#endif // DLIB_ALL_SOURCe_
 
 #define THREADS_PER_BLOCK 32
 #define MIN_GRIDDIM 8
