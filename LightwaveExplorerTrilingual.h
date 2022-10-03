a190c2237f0530d10d2299f2251af51c7ff5ee5b
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2022-10-03 23:08:32 +0200

    more cleanup, skeleton of SYCL version

--- LightwaveExplorerTrilingual.h
+++ LightwaveExplorerTrilingual.h
@@ -1,7 +1,9 @@
 #include <memory>
 #include "LightwaveExplorerUtilities.h"
 
-//define a language specific class and set of macros
+
+// the anonymous namespace contains a few helper functions that are identical
+// for all classes, taken out here to have less duplicated code
 namespace {
 	int fillRotationMatricies(simulationParameterSet* sCPU, cudaParameterSet* s) {
 		double cosT = cos((*sCPU).crystalTheta);
@@ -49,8 +51,74 @@ namespace {
 		(*s).isNonLinear = ((*sCPU).nonlinearSwitches[0] + (*sCPU).nonlinearSwitches[1]) > 0;
 		(*s).isUsingMillersRule = ((*sCPU).crystalDatabase[(*sCPU).materialIndex].nonlinearReferenceFrequencies[0]) != 0;
 	}
+
+	void finishConfiguration(simulationParameterSet* sCPU, cudaParameterSet* s) {
+		size_t beamExpansionFactor = 1;
+		if ((*s).isCylindric) {
+			beamExpansionFactor = 2;
+		}
+		//second polarization grids are to pointers within the first polarization
+		//to have contiguous memory
+		(*s).gridETime2 = (*s).gridETime1 + (*s).Ngrid;
+		(*s).workspace2 = (*s).workspace1 + (*s).NgridC;
+		(*s).gridPolarizationTime2 = (*s).gridPolarizationTime1 + (*s).Ngrid;
+		(*s).workspace2P = (*s).workspace1 + beamExpansionFactor * (*s).NgridC;
+		(*s).k2 = (*s).k1 + (*s).NgridC;
+		(*s).chiLinear2 = (*s).chiLinear1 + (*s).Nfreq;
+		(*s).fieldFactor2 = (*s).fieldFactor1 + (*s).Nfreq;
+		(*s).inverseChiLinear2 = (*s).inverseChiLinear1 + (*s).Nfreq;
+		(*s).gridRadialLaplacian2 = (*s).gridRadialLaplacian1 + (*s).Ngrid;
+		(*s).gridPropagationFactor1Rho2 = (*s).gridPropagationFactor1Rho1 + (*s).NgridC;
+		(*s).gridPolarizationFactor2 = (*s).gridPolarizationFactor1 + (*s).NgridC;
+		(*s).gridEFrequency1Next2 = (*s).gridEFrequency1Next1 + (*s).NgridC;
+		(*s).gridPropagationFactor2 = (*s).gridPropagationFactor1 + (*s).NgridC;
+		(*s).gridEFrequency2 = (*s).gridEFrequency1 + (*s).NgridC;
+
+		double firstDerivativeOperation[6] = { -1. / 60.,  3. / 20., -3. / 4.,  3. / 4.,  -3. / 20., 1. / 60. };
+		for (size_t i = 0; i < 6; i++) {
+			firstDerivativeOperation[i] *= (-2.0 / ((*s).Ngrid * (*s).dx));
+		}
+
+		//set nonlinearSwitches[3] to the number of photons needed to overcome bandgap
+		(*sCPU).nonlinearSwitches[3] = (int)ceil((*sCPU).bandGapElectronVolts * 241.79893e12 / (*sCPU).frequency1) - 2;
+		double plasmaParametersCPU[6] = { 0 };
+
+		if ((*sCPU).nonlinearAbsorptionStrength > 0.) {
+			(*s).hasPlasma = TRUE;
+			(*s).isNonLinear = TRUE;
+		}
+		else {
+			(*s).hasPlasma = FALSE;
+		}
+
+		if ((*s).forceLinear) {
+			(*s).hasPlasma = FALSE;
+			(*s).isNonLinear = FALSE;
+		}
+		plasmaParametersCPU[0] = (*sCPU).nonlinearAbsorptionStrength; //nonlinear absorption strength parameter
+		plasmaParametersCPU[1] = (*sCPU).drudeGamma; //gamma
+		if ((*sCPU).nonlinearAbsorptionStrength > 0.) {
+			plasmaParametersCPU[2] = (*sCPU).tStep * (*sCPU).tStep
+				* 2.817832e-08 / (1.6022e-19 * (*sCPU).bandGapElectronVolts * (*sCPU).effectiveMass); // (dt^2)*e* e / (m * band gap));
+		}
+		else {
+			plasmaParametersCPU[2] = 0;
+		}
+
+		memcpy((*s).chi2Tensor, (*sCPU).chi2Tensor, 18 * sizeof(double));
+		for (int j = 0; j < 18; j++) {
+			(*s).chi2Tensor[j] *= 2e-12; //go from d in pm/V to chi2 in m/V
+			if (j > 8) (*s).chi2Tensor[j] *= 2.0; //multiply cross-terms by 2 for consistency with convention
+		}
+		memcpy((*s).nonlinearSwitches, (*sCPU).nonlinearSwitches, 4 * sizeof(int));
+		memcpy((*s).chi3Tensor, (*sCPU).chi3Tensor, 81 * sizeof(double));
+		memcpy((*s).absorptionParameters, (*sCPU).absorptionParameters, 6 * sizeof(double));
+		memcpy((*s).plasmaParameters, plasmaParametersCPU, 6 * sizeof(double));
+		memcpy((*s).firstDerivativeOperation, firstDerivativeOperation, 6 * sizeof(double));
+	}
 }
 
+// define a language specific class and set of macros
 #ifdef __CUDACC__
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
@@ -237,13 +305,6 @@ public:
 			return 1;
 		}
 
-		(*s).fftPlan1DD2Z = fftPlan1DD2Z;
-		(*s).fftPlan1DZ2D = fftPlan1DZ2D;
-		(*s).fftPlanD2Z = fftPlanD2Z;
-		(*s).fftPlanZ2D = fftPlanZ2D;
-		(*s).doublePolfftPlan = doublePolfftPlan;
-
-
 		int memErrors = 0;
 		double* expGammaTCPU = new double[2 * (*s).Ntime];
 
@@ -289,65 +350,7 @@ public:
 		if (memErrors > 0) {
 			return memErrors;
 		}
-
-		//second polarization grids are to pointers within the first polarization
-		//to have contiguous memory
-		(*s).gridETime2 = (*s).gridETime1 + (*s).Ngrid;
-		(*s).workspace2 = (*s).workspace1 + (*s).NgridC;
-		(*s).gridPolarizationTime2 = (*s).gridPolarizationTime1 + (*s).Ngrid;
-		(*s).workspace2P = (*s).workspace1 + beamExpansionFactor * (*s).NgridC;
-		(*s).k2 = (*s).k1 + (*s).NgridC;
-		(*s).chiLinear2 = (*s).chiLinear1 + (*s).Nfreq;
-		(*s).fieldFactor2 = (*s).fieldFactor1 + (*s).Nfreq;
-		(*s).inverseChiLinear2 = (*s).inverseChiLinear1 + (*s).Nfreq;
-		(*s).gridRadialLaplacian2 = (*s).gridRadialLaplacian1 + (*s).Ngrid;
-		(*s).gridPropagationFactor1Rho2 = (*s).gridPropagationFactor1Rho1 + (*s).NgridC;
-		(*s).gridPolarizationFactor2 = (*s).gridPolarizationFactor1 + (*s).NgridC;
-		(*s).gridEFrequency1Next2 = (*s).gridEFrequency1Next1 + (*s).NgridC;
-		(*s).gridPropagationFactor2 = (*s).gridPropagationFactor1 + (*s).NgridC;
-		(*s).gridEFrequency2 = (*s).gridEFrequency1 + (*s).NgridC;
-
-		double firstDerivativeOperation[6] = { -1. / 60.,  3. / 20., -3. / 4.,  3. / 4.,  -3. / 20., 1. / 60. };
-		for (size_t i = 0; i < 6; i++) {
-			firstDerivativeOperation[i] *= (-2.0 / ((*s).Ngrid * (*s).dx));
-		}
-
-		//set nonlinearSwitches[3] to the number of photons needed to overcome bandgap
-		(*sCPU).nonlinearSwitches[3] = (int)ceil((*sCPU).bandGapElectronVolts * 241.79893e12 / (*sCPU).frequency1) - 2;
-		double plasmaParametersCPU[6] = { 0 };
-
-		if ((*sCPU).nonlinearAbsorptionStrength > 0.) {
-			(*s).hasPlasma = TRUE;
-			(*s).isNonLinear = TRUE;
-		}
-		else {
-			(*s).hasPlasma = FALSE;
-		}
-
-		if ((*s).forceLinear) {
-			(*s).hasPlasma = FALSE;
-			(*s).isNonLinear = FALSE;
-		}
-		plasmaParametersCPU[0] = (*sCPU).nonlinearAbsorptionStrength; //nonlinear absorption strength parameter
-		plasmaParametersCPU[1] = (*sCPU).drudeGamma; //gamma
-		if ((*sCPU).nonlinearAbsorptionStrength > 0.) {
-			plasmaParametersCPU[2] = (*sCPU).tStep * (*sCPU).tStep
-				* 2.817832e-08 / (1.6022e-19 * (*sCPU).bandGapElectronVolts * (*sCPU).effectiveMass); // (dt^2)*e* e / (m * band gap));
-		}
-		else {
-			plasmaParametersCPU[2] = 0;
-		}
-
-		memcpy((*s).chi2Tensor, (*sCPU).chi2Tensor, 18 * sizeof(double));
-		for (int j = 0; j < 18; j++) {
-			(*s).chi2Tensor[j] *= 2e-12; //go from d in pm/V to chi2 in m/V
-			if (j > 8) (*s).chi2Tensor[j] *= 2.0; //multiply cross-terms by 2 for consistency with convention
-		}
-		memcpy((*s).nonlinearSwitches, (*sCPU).nonlinearSwitches, 4 * sizeof(int));
-		memcpy((*s).chi3Tensor, (*sCPU).chi3Tensor, 81 * sizeof(double));
-		memcpy((*s).absorptionParameters, (*sCPU).absorptionParameters, 6 * sizeof(double));
-		memcpy((*s).plasmaParameters, plasmaParametersCPU, 6 * sizeof(double));
-		memcpy((*s).firstDerivativeOperation, firstDerivativeOperation, 6 * sizeof(double));
+		finishConfiguration(sCPU, s);
 		deviceMemcpy(dParamsDevice, s, sizeof(cudaParameterSet), HostToDevice);
 		return 0;
 	}
@@ -355,6 +358,7 @@ public:
 };
 #elif defined RUNONSYCL
 #include <CL/sycl.hpp>
+#include <oneapi/mkl.hpp>
 #define trilingual const auto 
 #define deviceFunction 
 #define RUNTYPE 2
@@ -368,13 +372,38 @@ public:
 #define withStream , stream
 #define activeDevice deviceSYCL
 class deviceSYCL {
+private:
+	bool configuredFFT;
+	bool isCylindric;
+
+	//to do
+	int fftPlanD2Z;
+	int fftPlanZ2D;
+	int fftPlan1DD2Z;
+	int fftPlan1DZ2D;
+	int doublePolfftPlan;
+
+	void fftDestroy() {
+		//to do
+	}
 public:
 	cl::sycl::queue stream;
+	cudaParameterSet* dParams;
+	cudaParameterSet* dParamsDevice;
+	simulationParameterSet* cParams;
 
 	deviceSYCL() {
+		configuredFFT = 0;
+		isCylindric = 0;
 		cl::sycl::default_selector syclDefaultSelector;
 		cl::sycl::queue initStream(syclDefaultSelector);
 		stream = initStream;
+	}
+
+	~deviceSYCL() {
+		fftDestroy();
+		deviceFree(dParamsDevice);
+		deviceCalloc((void**)&dParamsDevice, 1, sizeof(cudaParameterSet));
 	}
 
 	template<typename Function, typename... Args>
@@ -404,6 +433,146 @@ public:
 
 	void deviceFree(void* block) {
 		cl::sycl::free(block, stream);
+	}
+
+	//to do
+	void fftInitialize(cudaParameterSet* s) {
+		if (configuredFFT) {
+			fftDestroy();
+		}
+		isCylindric = 0;
+		/*(*s).CUDAStream = stream;
+		size_t workSize;
+		cufftPlan1d(&fftPlan1DD2Z, (int)(*s).Ntime, CUFFT_D2Z, 2 * (int)((*s).Nspace * (*s).Nspace2));
+		cufftPlan1d(&fftPlan1DZ2D, (int)(*s).Ntime, CUFFT_Z2D, 2 * (int)((*s).Nspace * (*s).Nspace2));
+		cufftSetStream(fftPlan1DD2Z, stream);
+		cufftSetStream(fftPlan1DZ2D, stream);
+		if ((*s).is3D) {
+			int cufftSizes1[] = { (int)(*s).Nspace2, (int)(*s).Nspace, (int)(*s).Ntime };
+			cufftCreate(&fftPlanD2Z);
+			cufftGetSizeMany(fftPlanD2Z, 3, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_D2Z, 2, &workSize);
+			cufftMakePlanMany(fftPlanD2Z, 3, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_D2Z, 2, &workSize);
+
+			cufftCreate(&fftPlanZ2D);
+			cufftGetSizeMany(fftPlanZ2D, 3, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_Z2D, 2, &workSize);
+			cufftMakePlanMany(fftPlanZ2D, 3, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_Z2D, 2, &workSize);
+		}
+		else {
+			int cufftSizes1[] = { (int)(*s).Nspace, (int)(*s).Ntime };
+
+			cufftCreate(&fftPlanD2Z);
+			cufftGetSizeMany(fftPlanD2Z, 2, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_D2Z, 2, &workSize);
+			cufftMakePlanMany(fftPlanD2Z, 2, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_D2Z, 2, &workSize);
+
+			cufftCreate(&fftPlanZ2D);
+			cufftGetSizeMany(fftPlanZ2D, 2, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_Z2D, 2, &workSize);
+			cufftMakePlanMany(fftPlanZ2D, 2, cufftSizes1, NULL, 0, 0, 0, 0, 0, CUFFT_Z2D, 2, &workSize);
+
+			if ((*s).isCylindric) {
+				isCylindric = 1;
+				int cufftSizes2[] = { 2 * (int)(*s).Nspace, (int)(*s).Ntime };
+				cufftCreate(&doublePolfftPlan);
+				cufftGetSizeMany(doublePolfftPlan, 2, cufftSizes2, NULL, 0, 0, 0, 0, 0, CUFFT_D2Z, 2, &workSize);
+				cufftMakePlanMany(doublePolfftPlan, 2, cufftSizes2, NULL, 0, 0, 0, 0, 0, CUFFT_D2Z, 2, &workSize);
+				cufftSetStream(doublePolfftPlan, stream);
+			}
+		}
+		cufftSetStream(fftPlanD2Z, stream);
+		cufftSetStream(fftPlanZ2D, stream);*/
+		configuredFFT = 1;
+	}
+
+	//to do
+	void fft(void* input, void* output, int type) {
+		switch (type) {
+		case 0:
+			//cufftExecD2Z(fftPlanD2Z, (cufftDoubleReal*)input, (cufftDoubleComplex*)output);
+			break;
+		case 1:
+			//cufftExecZ2D(fftPlanZ2D, (cufftDoubleComplex*)input, (cufftDoubleReal*)output);
+			break;
+		case 2:
+			//cufftExecD2Z(fftPlan1DD2Z, (cufftDoubleReal*)input, (cufftDoubleComplex*)output);
+			break;
+		case 3:
+			//cufftExecZ2D(fftPlan1DZ2D, (cufftDoubleComplex*)input, (cufftDoubleReal*)output);
+			break;
+		case 4:
+			//cufftExecD2Z(doublePolfftPlan, (cufftDoubleReal*)input, (cufftDoubleComplex*)output);
+			break;
+		}
+	}
+	void deallocateSet(cudaParameterSet* s) {
+		deviceFree((*s).gridETime1);
+		deviceFree((*s).workspace1);
+		deviceFree((*s).gridEFrequency1);
+		deviceFree((*s).gridPropagationFactor1);
+		if ((*s).isCylindric) {
+			deviceFree((*s).gridPropagationFactor1Rho1);
+			deviceFree((*s).gridRadialLaplacian1);
+		}
+		deviceFree((*s).gridPolarizationFactor1);
+		deviceFree((*s).gridEFrequency1Next1);
+		deviceFree((*s).k1);
+		deviceFree((*s).gridPolarizationTime1);
+		deviceFree((*s).expGammaT);
+		deviceFree((*s).chiLinear1);
+		deviceFree((*s).fieldFactor1);
+		deviceFree((*s).inverseChiLinear1);
+	}
+	int allocateSet(simulationParameterSet* sCPU, cudaParameterSet* s) {
+		dParams = s;
+		cParams = sCPU;
+		initializeDeviceParameters(sCPU, s);
+		fftInitialize(s);
+		int memErrors = 0;
+		double* expGammaTCPU = new double[2 * (*s).Ntime];
+
+		size_t beamExpansionFactor = 1;
+		if ((*s).isCylindric) {
+			beamExpansionFactor = 2;
+		}
+
+		fillRotationMatricies(sCPU, s);
+
+		//GPU allocations
+		//
+		// currently 8 large grids, meaning memory use is approximately
+		// 64 bytes per grid point (8 grids x 2 polarizations x 4ouble precision)
+		// plus a little bit for additional constants/workspaces/etc
+		memErrors += deviceCalloc((void**)&(*s).gridETime1, 2 * (*s).Ngrid, sizeof(double));
+		memErrors += deviceCalloc((void**)&(*s).gridPolarizationTime1, 2 * (*s).Ngrid, sizeof(double));
+		memErrors += deviceCalloc((void**)&(*s).workspace1, beamExpansionFactor * 2 * (*s).NgridC, sizeof(std::complex<double>));
+		memErrors += deviceCalloc((void**)&(*s).gridEFrequency1, 2 * (*s).NgridC, sizeof(std::complex<double>));
+		memErrors += deviceCalloc((void**)&(*s).gridPropagationFactor1, 2 * (*s).NgridC, sizeof(std::complex<double>));
+		memErrors += deviceCalloc((void**)&(*s).gridPolarizationFactor1, 2 * (*s).NgridC, sizeof(std::complex<double>));
+		memErrors += deviceCalloc((void**)&(*s).gridEFrequency1Next1, 2 * (*s).NgridC, sizeof(std::complex<double>));
+		memErrors += deviceCalloc((void**)&(*s).k1, 2 * (*s).NgridC, sizeof(std::complex<double>));
+
+		//cylindric sym grids
+		if ((*s).isCylindric) {
+			memErrors += deviceCalloc((void**)&(*s).gridPropagationFactor1Rho1, 4 * (*s).NgridC, sizeof(std::complex<double>));
+			memErrors += deviceCalloc((void**)&(*s).gridRadialLaplacian1, 4 * (*s).Ngrid, sizeof(std::complex<double>));
+		}
+
+		//smaller helper grids
+		memErrors += deviceCalloc((void**)&(*s).expGammaT, 2 * (*s).Ntime, sizeof(double));
+		memErrors += deviceCalloc((void**)&(*s).chiLinear1, 2 * (*s).Nfreq, sizeof(std::complex<double>));
+		memErrors += deviceCalloc((void**)&(*s).fieldFactor1, 2 * (*s).Nfreq, sizeof(double));
+		memErrors += deviceCalloc((void**)&(*s).inverseChiLinear1, 2 * (*s).Nfreq, sizeof(double));
+		for (size_t i = 0; i < (*s).Ntime; i++) {
+			expGammaTCPU[i] = exp((*s).dt * i * (*sCPU).drudeGamma);
+			expGammaTCPU[i + (*s).Ntime] = exp(-(*s).dt * i * (*sCPU).drudeGamma);
+		}
+		deviceMemcpy((*s).expGammaT, expGammaTCPU, 2 * sizeof(double) * (*s).Ntime, HostToDevice);
+		delete[] expGammaTCPU;
+		(*sCPU).memoryError = memErrors;
+		if (memErrors > 0) {
+			return memErrors;
+		}
+		finishConfiguration(sCPU, s);
+		deviceMemcpy(dParamsDevice, s, sizeof(cudaParameterSet), HostToDevice);
+		return 0;
 	}
 };
 #else
@@ -447,8 +616,17 @@ public:
 	simulationParameterSet* cParams;
 	cudaParameterSet* dParamsDevice;
 	deviceCPU() {
+		stream = 0;
+		dParams = NULL;
+		cParams = NULL;
+		dParamsDevice = NULL;
 		configuredFFT = 0;
 		isCylindric = 0;
+		fftPlanD2Z = 0;
+		fftPlanZ2D = 0;
+		fftPlan1DD2Z = 0;
+		fftPlan1DZ2D = 0;
+		doublePolfftPlan = 0;
 		dParamsDevice = &dParamslocal;
 	}
 
@@ -599,69 +777,9 @@ public:
 		if (memErrors > 0) {
 			return memErrors;
 		}
-
-		//second polarization grids are to pointers within the first polarization
-		//to have contiguous memory
-		(*s).gridETime2 = (*s).gridETime1 + (*s).Ngrid;
-		(*s).workspace2 = (*s).workspace1 + (*s).NgridC;
-		(*s).gridPolarizationTime2 = (*s).gridPolarizationTime1 + (*s).Ngrid;
-		(*s).workspace2P = (*s).workspace1 + beamExpansionFactor * (*s).NgridC;
-		(*s).k2 = (*s).k1 + (*s).NgridC;
-		(*s).chiLinear2 = (*s).chiLinear1 + (*s).Nfreq;
-		(*s).fieldFactor2 = (*s).fieldFactor1 + (*s).Nfreq;
-		(*s).inverseChiLinear2 = (*s).inverseChiLinear1 + (*s).Nfreq;
-		(*s).gridRadialLaplacian2 = (*s).gridRadialLaplacian1 + (*s).Ngrid;
-		(*s).gridPropagationFactor1Rho2 = (*s).gridPropagationFactor1Rho1 + (*s).NgridC;
-		(*s).gridPolarizationFactor2 = (*s).gridPolarizationFactor1 + (*s).NgridC;
-		(*s).gridEFrequency1Next2 = (*s).gridEFrequency1Next1 + (*s).NgridC;
-		(*s).gridPropagationFactor2 = (*s).gridPropagationFactor1 + (*s).NgridC;
-		(*s).gridEFrequency2 = (*s).gridEFrequency1 + (*s).NgridC;
-
-		double firstDerivativeOperation[6] = { -1. / 60.,  3. / 20., -3. / 4.,  3. / 4.,  -3. / 20., 1. / 60. };
-		for (size_t i = 0; i < 6; i++) {
-			firstDerivativeOperation[i] *= (-2.0 / ((*s).Ngrid * (*s).dx));
-		}
-
-		//set nonlinearSwitches[3] to the number of photons needed to overcome bandgap
-		(*sCPU).nonlinearSwitches[3] = (int)ceil((*sCPU).bandGapElectronVolts * 241.79893e12 / (*sCPU).frequency1) - 2;
-		double plasmaParametersCPU[6] = { 0 };
-
-		if ((*sCPU).nonlinearAbsorptionStrength > 0.) {
-			(*s).hasPlasma = TRUE;
-			(*s).isNonLinear = TRUE;
-		}
-		else {
-			(*s).hasPlasma = FALSE;
-		}
-
-		if ((*s).forceLinear) {
-			(*s).hasPlasma = FALSE;
-			(*s).isNonLinear = FALSE;
-		}
-		plasmaParametersCPU[0] = (*sCPU).nonlinearAbsorptionStrength; //nonlinear absorption strength parameter
-		plasmaParametersCPU[1] = (*sCPU).drudeGamma; //gamma
-		if ((*sCPU).nonlinearAbsorptionStrength > 0.) {
-			plasmaParametersCPU[2] = (*sCPU).tStep * (*sCPU).tStep
-				* 2.817832e-08 / (1.6022e-19 * (*sCPU).bandGapElectronVolts * (*sCPU).effectiveMass); // (dt^2)*e* e / (m * band gap));
-		}
-		else {
-			plasmaParametersCPU[2] = 0;
-		}
-
-		memcpy((*s).chi2Tensor, (*sCPU).chi2Tensor, 18 * sizeof(double));
-		for (int j = 0; j < 18; j++) {
-			(*s).chi2Tensor[j] *= 2e-12; //go from d in pm/V to chi2 in m/V
-			if (j > 8) (*s).chi2Tensor[j] *= 2.0; //multiply cross-terms by 2 for consistency with convention
-		}
-		memcpy((*s).nonlinearSwitches, (*sCPU).nonlinearSwitches, 4 * sizeof(int));
-		memcpy((*s).chi3Tensor, (*sCPU).chi3Tensor, 81 * sizeof(double));
-		memcpy((*s).absorptionParameters, (*sCPU).absorptionParameters, 6 * sizeof(double));
-		memcpy((*s).plasmaParameters, plasmaParametersCPU, 6 * sizeof(double));
-		memcpy((*s).firstDerivativeOperation, firstDerivativeOperation, 6 * sizeof(double));
-
+		finishConfiguration(sCPU, s);
 		deviceMemcpy(dParamsDevice, s, sizeof(cudaParameterSet), HostToDevice);
 		return 0;
 	}
-
 };
 #endif
