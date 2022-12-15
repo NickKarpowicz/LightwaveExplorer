eab4a2a73fc178a09ae900e81e327c0ea9f94f72
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2022-12-15 21:50:55 +0100

    gtk version icon

--- LightwaveExplorerGTK/resource.h
+++ LightwaveExplorerGTK/resource.h
@@ -2,13 +2,19 @@
 // Microsoft Visual C++ generated include file.
 // Used by LightwaveExplorerGTK.rc
 //
-#define IDI_ICON1                       101
+#define IDI_ICON1                       111
+#define IDI_ICON2                       112
+#define IDI_ICON3                       113
+#define IDI_ICON4                       114
+#define IDI_ICON5                       115
+#define IDI_ICON6                       116
+#define IDI_ICON7                       119
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        120
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
