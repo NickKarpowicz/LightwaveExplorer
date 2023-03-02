dee2f209a132c1c27b8ae8a9749928017bfc04f3
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2023-03-02 12:07:45 +0100

    cluster script

--- BuildResources/compileCommandLineLWEfromRepos.sh
+++ BuildResources/compileCommandLineLWEfromRepos.sh
@@ -22,7 +22,7 @@ module purge
 module load gcc/11
 module load cuda/11.6
 module load mkl/2022.2
-nvcc -gencode=arch=compute_70,code=\"sm_70,compute_70\" -gencode=arch=compute_75,code=\"sm_75,compute_75\" -gencode=arch=compute_80,code=\"sm_80,compute_80\" -x cu -I$MKL_HOME/include -I$MKL_HOME/include/fftw -I../dlib --machine 64 -Xcompiler -std=c++17 -use_fast_math -O3 -L$MKL_HOME/lib/intel64 -lcufft -lnvidia-ml -lmkl_sequential -lmkl_core -lmkl_intel_lp64 -o lwe LightwaveExplorerCore.cu LightwaveExplorerDevices/LightwaveExplorerUtilities.o LightwaveExplorerDevices/DlibLibraryComponents.cpp
+nvcc -gencode=arch=compute_70,code=\"sm_70,compute_70\" -allow-unsupported-compiler -gencode=arch=compute_75,code=\"sm_75,compute_75\" -gencode=arch=compute_80,code=\"sm_80,compute_80\" -x cu -I$MKL_HOME/include -I$MKL_HOME/include/fftw -I../dlib --machine 64 -Xcompiler -std=c++17 -use_fast_math -O3 -L$MKL_HOME/lib/intel64 -lcufft -lnvidia-ml -lmkl_sequential -lmkl_core -lmkl_intel_lp64 -o lwe LightwaveExplorerCore.cu LightwaveExplorerDevices/LightwaveExplorerUtilities.o LightwaveExplorerDevices/DlibLibraryComponents.cpp
 
 echo "Cleaning up "
 cp lwe ../lwe
