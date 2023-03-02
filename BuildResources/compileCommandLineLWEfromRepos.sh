e6434b4d9d47b6f9acdd1476b6f1bc9ed8856209
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2023-03-02 12:52:15 +0100

    continuing battle with cluster

--- BuildResources/compileCommandLineLWEfromRepos.sh
+++ BuildResources/compileCommandLineLWEfromRepos.sh
@@ -9,7 +9,7 @@
 # load modules (you probably don't need this if you're not on the cluster
 # but you will need to set MKL_HOME to the location of MKL)
 module purge
-module load gcc/12
+module load gcc/11
 module load mkl/2022.2
 module load cuda/11.6
 git clone https://github.com/NickKarpowicz/LightwaveExplorer
@@ -17,7 +17,7 @@ git clone https://github.com/davisking/dlib
 
 cd LightwaveExplorer
 echo "Starting to compile, this will take a couple of minutes... "
-nvcc -gencode=arch=compute_70,code=\"sm_70,compute_70\" -allow-unsupported-compiler -gencode=arch=compute_75,code=\"sm_75,compute_75\" -gencode=arch=compute_80,code=\"sm_80,compute_80\" -x cu -I$MKL_HOME/include -I$MKL_HOME/include/fftw -I../dlib --machine 64 -Xcompiler -std=c++17 -use_fast_math -O3 -L$MKL_HOME/lib/intel64 -lcufft -lnvidia-ml -lmkl_sequential -lmkl_core -lmkl_intel_lp64 -o lwe LightwaveExplorerCore.cu LightwaveExplorerDevices/DlibLibraryComponents.cpp LightwaveExplorerDevices/LightwaveExplorerUtilities.cpp
+nvcc -gencode=arch=compute_70,code=\"sm_70,compute_70\" -gencode=arch=compute_75,code=\"sm_75,compute_75\" -gencode=arch=compute_80,code=\"sm_80,compute_80\" -x cu -I$MKL_HOME/include -I$MKL_HOME/include/fftw -I../dlib --machine 64 -Xcompiler -std=c++17 -use_fast_math -O3 -L$MKL_HOME/lib/intel64 -lcufft -lnvidia-ml -lmkl_sequential -lmkl_core -lmkl_intel_lp64 -o lwe LightwaveExplorerCore.cu LightwaveExplorerDevices/DlibLibraryComponents.cpp LightwaveExplorerDevices/LightwaveExplorerUtilities.cpp
 
 echo "Cleaning up "
 cp lwe ../lwe
