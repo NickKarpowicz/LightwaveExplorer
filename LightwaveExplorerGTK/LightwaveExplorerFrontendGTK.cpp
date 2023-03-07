2e1ef0dbb19aaa9d0074669728f4df07b972f18d
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2023-03-07 18:15:19 +0100

    Move SYCL to devices folder

--- LightwaveExplorerGTK/LightwaveExplorerFrontendGTK.cpp
+++ LightwaveExplorerGTK/LightwaveExplorerFrontendGTK.cpp
@@ -13,7 +13,7 @@
 #include "../LightwaveExplorerCore.cuh"
 #endif
 #ifdef _WIN32
-#include "../LightwaveExplorerSYCL/LightwaveExplorerSYCL.h"
+#include "../LightwaveExplorerDevices/LightwaveExplorerSYCL.h"
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
