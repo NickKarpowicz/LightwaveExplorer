0ce2d07574805d6c24962a9e3d1bdbefef1b4476
Nicholas Karpowicz <nick.karpowicz@gmail.com>
2023-08-13 20:16:29 +0200

    Make building on old distros work again

--- BuildResources/makeLinuxIcons.sh
+++ BuildResources/makeLinuxIcons.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 # Path to the 512x512 pixel PNG icon
 icon_path="../BuildResources/AppImage/ico512.png"
-sizes=("256x256" "128x128" "64x64" "48x48" "32x32" "24x24" "16x16")
 mkdir -p icons/hicolor/512x512/apps
 cp ${icon_path} icons/hicolor/512x512/apps/io.github.NickKarpowicz.LightwaveExplorer.png
-for size in "${sizes[@]}"; do
-  mkdir -p icons/hicolor/${size}/apps
-  output_path="icons/hicolor/${size}/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
-  convert "${icon_path}" -resize "${size}" "${output_path}"
-done
+mkdir -p icons/hicolor/256x256/apps
+convert "${icon_path}" -resize "256x256" "icons/hicolor/256x256/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
 
-# # Update icon cache
-# sudo gtk-update-icon-cache /usr/share/icons/hicolor
+mkdir -p icons/hicolor/128x128/apps
+convert "${icon_path}" -resize "128x128" "icons/hicolor/128x128/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
+
+mkdir -p icons/hicolor/64x64/apps
+convert "${icon_path}" -resize "64x64" "icons/hicolor/64x64/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
+
+mkdir -p icons/hicolor/48x48/apps
+convert "${icon_path}" -resize "48x48" "icons/hicolor/48x48/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
+
+mkdir -p icons/hicolor/32x32/apps
+convert "${icon_path}" -resize "32x32" "icons/hicolor/32x32/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
+
+mkdir -p icons/hicolor/24x24/apps
+convert "${icon_path}" -resize "24x24" "icons/hicolor/24x24/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
+
+mkdir -p icons/hicolor/16x16/apps
+convert "${icon_path}" -resize "16x16" "icons/hicolor/16x16/apps/io.github.NickKarpowicz.LightwaveExplorer.png"
