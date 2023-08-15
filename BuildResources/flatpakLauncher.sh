2fef8c067d19b4b920a430588ca51aa751fde472
NickKarpowicz <nick.karpowicz@gmail.com>
2023-08-15 20:55:07 +0200

    try conditional launcher with both built in same cmake command

--- BuildResources/flatpakLauncher.sh
+++ BuildResources/flatpakLauncher.sh
@@ -2,14 +2,8 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/opt/cuda/lib64
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-NVML="libnvidia-ml.so.1"
-OLD_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
-
-
-for dir in $(echo $OLD_LD_LIBRARY_PATH | tr ":" "\n"); do
-    if [ -f "$dir/$LIBRARY" ]; then
-        echo "Sorry, this was just a test."
-        exit 0
-    fi
-done
-exec "${script_dir}/LightwaveExplorer"
+if /sbin/ldconfig -p | grep libnvidia-ml; then
+    exec "${script_dir}/LightwaveExplorer"
+else
+    exec "${script_dir}/LightwaveExplorerNoCuda"
+fi
