810219aa56ab8e8a4db53c1f46d94312d4dab826
NickKarpowicz <nick.karpowicz@gmail.com>
2023-08-15 08:48:05 +0200

    progress towards oneapi in flatpak

--- BuildResources/makeLaunchScript.sh
+++ BuildResources/makeLaunchScript.sh
@@ -3,4 +3,5 @@ touch LightwaveExplorerLauncher.sh
 echo "#!/bin/bash -l" >>  LightwaveExplorerLauncher.sh
 echo ". ${ONEAPI_ROOT}/setvars.sh" >> LightwaveExplorerLauncher.sh
 echo "export SYCL_CACHE_PERSISTENT=1" >>  LightwaveExplorerLauncher.sh
-echo "exec /usr/local/bin/LightwaveExplorer" >>  LightwaveExplorerLauncher.sh
+echo 'script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"' >> LightwaveExplorerLauncher.sh
+echo '"exec "${script_dir}/LightwaveExplorer"' >>  LightwaveExplorerLauncher.sh
