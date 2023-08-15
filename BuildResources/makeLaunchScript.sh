5b82f888bd9e0bb959960d2b3752a7327229941c
NickKarpowicz <nick.karpowicz@gmail.com>
2023-08-15 18:57:54 +0200

    more flatpak

--- BuildResources/makeLaunchScript.sh
+++ BuildResources/makeLaunchScript.sh
@@ -3,6 +3,6 @@ touch LightwaveExplorerLauncher.sh
 echo "#!/bin/bash -l" >>  LightwaveExplorerLauncher.sh
 echo ". ${ONEAPI_ROOT}/setvars.sh" >> LightwaveExplorerLauncher.sh
 echo "export SYCL_CACHE_PERSISTENT=1" >>  LightwaveExplorerLauncher.sh
-echo "printenv | grep HOME" >> LightwaveExplorerLauncher.sh
+echo '"export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/opt/cuda/lib64' >> LightwaveExplorerLauncher.sh
 echo 'script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"' >> LightwaveExplorerLauncher.sh
 echo 'exec "${script_dir}/LightwaveExplorer"' >>  LightwaveExplorerLauncher.sh
