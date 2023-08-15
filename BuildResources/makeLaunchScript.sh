340ea99b165cd61bf79c0904e1e2fac67f45d9ec
NickKarpowicz <nick.karpowicz@gmail.com>
2023-08-15 09:35:28 +0200

    temporary change for testing flatpak generation

--- BuildResources/makeLaunchScript.sh
+++ BuildResources/makeLaunchScript.sh
@@ -3,5 +3,6 @@ touch LightwaveExplorerLauncher.sh
 echo "#!/bin/bash -l" >>  LightwaveExplorerLauncher.sh
 echo ". ${ONEAPI_ROOT}/setvars.sh" >> LightwaveExplorerLauncher.sh
 echo "export SYCL_CACHE_PERSISTENT=1" >>  LightwaveExplorerLauncher.sh
+echo "printenv | grep HOME" >> LightwaveExplorerLauncher.sh
 echo 'script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"' >> LightwaveExplorerLauncher.sh
 echo 'exec "${script_dir}/LightwaveExplorer"' >>  LightwaveExplorerLauncher.sh
