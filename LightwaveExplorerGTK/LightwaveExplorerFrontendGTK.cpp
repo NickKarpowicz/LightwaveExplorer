b681a9bfbf3a0d692f01d6437dd1528357b05b6a
NickKarpowicz <nick.karpowicz@gmail.com>
2023-08-16 19:46:35 +0200

    try setting gtk_application_new name

--- LightwaveExplorerGTK/LightwaveExplorerFrontendGTK.cpp
+++ LightwaveExplorerGTK/LightwaveExplorerFrontendGTK.cpp
@@ -2217,7 +2217,7 @@ void fittingThread(int pulldownSelection, bool use64bitFloatingPoint) {
 }
 
 int main(int argc, char **argv){
-    GtkApplication* app = gtk_application_new(0, static_cast<GApplicationFlags>(0));
+    GtkApplication* app = gtk_application_new("io.github.NickKarpowicz.LightwaveExplorer", static_cast<GApplicationFlags>(0));
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     return g_application_run(G_APPLICATION(app), argc, argv);
 }
