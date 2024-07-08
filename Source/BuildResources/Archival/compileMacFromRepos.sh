6144aa5f49603f52dbbeae73a4a2e39dac95b39c
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2024-07-08 15:02:59 +0200

    Clean up cmakelists and buildresources

--- Source/BuildResources/Archival/compileMacFromRepos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -l
-git clone https://github.com/NickKarpowicz/LightwaveExplorer
-git clone https://github.com/davisking/dlib
-cd LightwaveExplorer
-chmod +x BuildResources/makeMacApp.sh
-./BuildResources/makeMacApp.sh
--- Source/BuildResources/makeMacAppQt.sh
+++ Source/BuildResources/makeMacAppQt.sh
@@ -24,6 +24,7 @@ mkdir $APP/Contents/Resources/share
 #copy in the databases and icons
 cp CrystalDatabase.txt $APP/Contents/Resources
 cp Source/BuildResources/DefaultValues.ini $APP/Contents/Resources
+cp Source/BuildResources/Licenses.txt $APP/Contents/Resources
 cp Source/BuildResources/AppIcon.icns $APP/Contents/Resources
 
 #Functions:
