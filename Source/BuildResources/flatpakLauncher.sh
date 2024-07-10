0ca0d92ab5c9ca930e161c0038117aca4af89a2c
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2024-07-10 19:17:19 +0200

    update cmakelists

--- Source/BuildResources/flatpakLauncher.sh
+++ Source/BuildResources/flatpakLauncher.sh
@@ -5,5 +5,6 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if /sbin/ldconfig -p | grep -q libnvidia-ml; then
     exec "${script_dir}/LightwaveExplorer"
 else
+    . /app/opt/intel/oneapi/setvars.sh
     exec "${script_dir}/LightwaveExplorerNoCuda"
 fi
