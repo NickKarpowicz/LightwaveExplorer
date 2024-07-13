570d0ae776598e2c85bcb3aefec7adbfeffe7c5e
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2024-07-13 09:17:00 +0200

    working on flatpak

--- Source/BuildResources/flatpakLauncher.sh
+++ Source/BuildResources/flatpakLauncher.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/opt/cuda/lib64
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if /sbin/ldconfig -p | grep -q libnvidia-ml; then
     exec "${script_dir}/LightwaveExplorer"
 else
-    . /app/opt/intel/oneapi/setvars.sh
     exec "${script_dir}/LightwaveExplorerNoCuda"
 fi
