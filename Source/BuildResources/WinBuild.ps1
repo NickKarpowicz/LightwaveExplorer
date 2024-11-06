a150b0547fb303a63676e365a8d3e8c3b1d437c9
NickKarpowicz <nick.karpowicz@gmail.com>
2024-11-06 17:52:28 +0100

    add archive generation to the WinBuild script

--- Source/BuildResources/WinBuild.ps1
+++ Source/BuildResources/WinBuild.ps1
@@ -12,3 +12,13 @@ cmake --build . --config Release
 
 cmake --fresh .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_CUDA_ARCHITECTURES="75;86"
 cmake --build . --config Release
+
+Copy-Item -Path Release -Destination LightwaveExplorerWin64 -Recurse 
+Remove-Item .\LightwaveExplorerWin64\LightwaveExplorerSYCL.exp -Force
+Remove-Item .\LightwaveExplorerWin64\LightwaveExplorerSYCL.lib
+
+if ($args.Count -gt 0){
+    $sevenZipPath = $args[0]
+    & $sevenZipPath a -tzip LightwaveExplorerWin64.zip LightwaveExplorerWin64\*
+}
+
