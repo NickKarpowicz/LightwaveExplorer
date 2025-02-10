a5b56c595f594a1627daf9451b206d9e5651ed29
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-02-10 07:53:28 +0100

    remove oneapi setvars from flatpak launcher

--- Source/BuildResources/flatpakLauncher.sh
+++ Source/BuildResources/flatpakLauncher.sh
@@ -3,6 +3,5 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if /sbin/ldconfig -p | grep -q libnvidia-ml; then
     exec "${script_dir}/LightwaveExplorer"
 else
-    . /app/opt/intel/oneapi/setvars.sh
     exec "${script_dir}/LightwaveExplorerNoCuda"
 fi
