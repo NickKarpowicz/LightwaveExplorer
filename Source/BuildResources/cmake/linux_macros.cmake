c63b379c085d9ffff07a5a460399a123a50cfe47
Nick Karpowicz <101142784+NickKarpowicz@users.noreply.github.com>
2025-04-08 07:19:00 +0200

    build with onemath 0.7

--- Source/BuildResources/cmake/linux_macros.cmake
+++ Source/BuildResources/cmake/linux_macros.cmake
@@ -98,7 +98,7 @@ macro(add_oneapi_interfaces)
     FetchContent_Declare(
             onemkl_interface_library
             GIT_REPOSITORY https://github.com/uxlfoundation/oneMath.git
-            GIT_TAG v0.6
+            GIT_TAG v0.7
     )
     FetchContent_MakeAvailable(onemkl_interface_library)
 
