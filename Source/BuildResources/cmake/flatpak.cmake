f9cc1d615ef42c49c426f81611197126fd00242f
Nick Karpowicz <nicholas.karpowicz@mpq.mpg.de>
2026-02-05 14:46:29 +0100

    third try to link gomp [no ci]

--- Source/BuildResources/cmake/flatpak.cmake
+++ Source/BuildResources/cmake/flatpak.cmake
@@ -47,7 +47,7 @@ target_compile_options(LightwaveExplorerNoCuda PRIVATE ${OpenMP_CXX_FLAGS} -DNOS
 target_link_libraries(LightwaveExplorerNoCuda Qt6::Widgets Qt6::DBus)
 target_link_libraries(LightwaveExplorerNoCuda ${CAIRO_LIBRARIES})
 target_link_libraries(LightwaveExplorerNoCuda miniz::miniz)
-target_link_libraries(LightwaveExplorer -lgomp)
+target_link_libraries(LightwaveExplorerNoCuda -lgomp)
 
 install(TARGETS ${EXECUTABLE_NAME} LightwaveExplorerNoCuda DESTINATION bin)
 install(PROGRAMS ${CMAKE_SOURCE_DIR}/Source/BuildResources/flatpakLauncher.sh DESTINATION bin)
