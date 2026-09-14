41cd5c27b7186f0c3c95e60c62e288024be3f6f7
Florian Lindinger <53214402+FlorianLindinger@users.noreply.github.com>
2026-09-14 17:26:30 +0200

    added zip rename support

--- Source/Python/src/LightwaveExplorer/LightwaveExplorer.py
+++ Source/Python/src/LightwaveExplorer/LightwaveExplorer.py
@@ -94,8 +94,7 @@ class lightwaveExplorerResult:
         if filePath.endswith(".zip"):
             isZip = True
             archive = zipfile.ZipFile(filePath, "r")
-            directory, archiveName = os.path.split(filePath)
-            fileBase, baseExtension = os.path.splitext(archiveName)
+            fileBase = next(name for name in archive.namelist() if name.lower().endswith(".txt"))[:-4]
             settingsFile = archive.open(fileBase + ".txt")
             settingsFile = io.TextIOWrapper(settingsFile, "utf-8")
 
